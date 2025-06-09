a5f3ad5964964e2f87fe54fc00b772a384e451ef
Kauã Lorenzi <157428596+Kilmo0@users.noreply.github.com>
2025-06-09 15:09:04 -0300

    Adicionando Pastas

--- Main.py
+++ Main.py
@@ -5,32 +5,46 @@ import time
 import webbrowser
 
 
-
 numerosPE = Path("Dados/Whatsapp/Numeros.txt")
 maincontact = Path("Dados/Whatsapp/mainwhatsapp.txt")
 mensagemfile = Path("Dados/Whatsapp/mensagem.txt")
-imagem = Path('Dados/Imagem/imagem.jpeg').resolve()
+imagem = Path('Dados/Imagem/Imagem.jpeg').resolve()
+
+if not imagem.exists():
+    print('Opa, bem-vindo, parece que é sua primeira vez usando o ZEM')
+    time.sleep(3)
+    print('Aguarde um minutinho que iremos criar algumas pastas')
+    arquivostxt = [numerosPE, maincontact, mensagemfile, imagem]
+    dadospath = Path('Dados')
+    dadospath.mkdir(exist_ok=True)
+    (dadospath / 'Imagem').mkdir(exist_ok=True)
+    (dadospath / 'Whatsapp').mkdir(exist_ok=True)
+    input('Precione enter para continuar, será aberto um video tutorial')
+    webbrowser.open('https://youtu.be/9je5k27rRMk')
+    for a in arquivostxt:
+        with open(a, 'w') as arquivo:
+            arquivo.write('Escreva aqui, de acordo com o video')
+
 
 print('-'*30)
 print('Seja bem vindo ao progrmaa ZEM')
 time.sleep(2)
 print('='*30)
-print('='*30)
 print('Você já usou o programa ou é a primeira vez?')
 
 while True:
-    inicio = input('Digite 1 Caso já usou o programa\n digite 2 caso nunca usou')
+    inicio = input('Digite 1 Caso já usou o programa\ndigite 2 caso nunca usou\n')
     try:
         match inicio:
             case '1':
                 print('Vamos começar')
                 time.sleep(1)
-                print('='*30)*5
+                print('='*30)
                 
                 break
             case '2':
                 print('Certo, iremos abrir um video ensinando como usar')
-                webbrowser.open('https://www.youtube.com/watch?v=ESx_hy1n7HA')
+                webbrowser.open('https://youtu.be/9je5k27rRMk')
                 input('Precione enter para continuar')
                 break
     except:
@@ -67,5 +81,6 @@ while True:
         ZEM.backmain(contactmain)
         time.sleep(random.randint(1,8))
 
-    input('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    input('Digite Enter para finalizar o programa')
+    break
     
